a01da2ec64e8bb46e3cb58891a1daed3e4b36c32
navroop777 <navroop777@yahoo.com>
2019-11-03 14:32:37 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,17 +4,20 @@ students =[]
 
 # create class
 class Student:
+    # contructor
     # self need to be added
-    def add_student(self, name, student_id=None):
+    def __init__(self, name, student_id=None):
         # add random student id if none provided
         if student_id is None:
             student_id = random.randrange(0, math.pow(2, 24) - 1)
-        #     crete a dict
+        #     create a dict
         student = {"name": name, "student_id": student_id}
         # add dict to the list
         students.append(student)
 
-student = Student()
-student.add_student("mark")
+    def __str__(self):
+        return "Student"
 
-print(students)
+student_mark = Student("mark")
+
+print(student_mark)
