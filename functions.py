0d82acc2608616fd139635e59f2f6a63062b0e27
navroop777 <navroop777@yahoo.com>
2019-11-03 13:06:01 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,10 +8,12 @@ def get_students_titlecase():
         students_titlecase.append(student["name"].title())
     return students_titlecase
 
+
 # print the student title case
 def print_students_titlecase():
     students_titlecase = get_students_titlecase()
     print(students_titlecase)
+
 
 # add student to the list with student id optional
 def add_student(name, student_id= None):
@@ -21,6 +23,18 @@ def add_student(name, student_id= None):
     student = {"name": name, "student_id": student_id}
     students.append(student)
 
+# any number ags
+def var_args(name, *args):
+    print(name)
+    print(args)
+
+
+def kvar_args(name, **kwargs):
+    print(name)
+    print(kwargs["description"], kwargs["id"])
+
+
+
 student_list = get_students_titlecase()
 
 add_student("john")
@@ -29,3 +43,5 @@ add_student("john3")
 add_student("john4")
 
 print(students)
+var_args("test", "user", "new",  True)
+kvar_args("don", description ="user", id ="id_123")
